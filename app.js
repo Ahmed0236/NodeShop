fff524c1c3729d2670bed981415dd2169cc65649
ahmed0236 <ahmedanwar5295@gmail.com>
2022-12-26 18:15:58 -0500

    added deployment code

---
+++ app.js
@@ -0,0 +1,135 @@
+const path = require("path");
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const { v4: uuidv4 } = require("uuid");
+
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+const authRouters = require("./routes/auth.js");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+//AWS
+// const MONGODB_URI =
+//   "mongodb+srv://ahmed:teradata123@cluster0.ywg6xca.mongodb.net/shop?retryWrites=true&w=majority";
+
+//GCP
+// const MONGODB_URI =
+//   "mongodb+srv://ahmed:teradata123@cluster0.v3wmtkn.mongodb.net/shop?retryWrites=true&w=majority";
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.v3wmtkn.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
+const csrfProtection = csrf();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.set("view engine", "ejs"); // compile dynamic views with pug engine
+app.set("views", "views"); //where to find these views
+app.set("trust proxy", true);
+
+const bodyParser = require("body-parser");
+const e = require("connect-flash");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+
+app.use(helmet());
+app.use(compression());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err)); //in async use next
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRouters);
+
+app.get("/500", errorController.get500);
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render("500", {
+    pageTitle: "Error!!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
